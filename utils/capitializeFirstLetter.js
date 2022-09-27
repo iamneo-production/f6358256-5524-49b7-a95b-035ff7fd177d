8ccdb7e5dbc1ff61de4736b1615e79cc5e5a0288
YashTariyal <yashtariyal4@gmail.com>
2022-09-27 17:28:38 +0000

    final-7

---
+++ utils/capitializeFirstLetter.js
@@ -0,0 +1,4 @@
+export default function capitalizeFirstLetter(s) {
+    return (s && s[0].toUpperCase() + s.slice(1)) || s
+  }
+  
---
+++ utils/currencyFormatter.js
@@ -0,0 +1,9 @@
+export default function formatter(value) {
+    const options = {
+      style: 'currency',
+      currency: 'INR',
+      minimumFractionDigits: 2,
+    }
+    return new Intl.NumberFormat('en-IN', options).format(value)
+  }
+  
